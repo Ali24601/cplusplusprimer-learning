0d4e9ee96941b8dbffa87e3e20cea33549958bcb
Ali24601 <425032585@qq.com>
2020-09-17 10:47:41 +0800

    Update create_primestable.cpp

--- primes/create_primestable.cpp
+++ primes/create_primestable.cpp
@@ -1,3 +1,7 @@
+/*INT_MAX	time use:922821ms
+对于[primes[lastpeerid]*primes[lastpeerid],primes[lastpeerid+1]*primes[lastpeerid+1])中间的每个数，依次用
+primes[0:lastpeerid]试探是否可以整除。效果好于用primes[lastpeerid:0]试探是否可以整除（好很多）
+*/
 class Solution {
 public:
 	vector<int> findprime()
